8bf41a2e49fb3d23b0c499358b35ea2ffe996cb7
mcl <1120664311@qq.com>
2022-08-19 11:02:15 +0800

    feat: init eslint

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.enableVersioning({
     type: VersioningType.URI,
   });
