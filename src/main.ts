cd09aaa5059a6c2e6322645c7579df27631bb70b
mcl <1120664311@qq.com>
2022-08-22 15:32:18 +0800

    feat: add API doc config

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,31 @@
-import { VersioningType } from '@nestjs/common';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
-  //success 响应格式处理
+  // success 响应格式处理
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 创建文档
+  generateDocument(app);
 
+  // 添加热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
   await app.listen(3000);
 }
 bootstrap();
