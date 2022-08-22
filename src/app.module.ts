2d47d1302b55a48f2b9ac98de0119b35ae175043
mcl <1120664311@qq.com>
2022-08-22 15:45:39 +0800

    feat: 调整 utils 文件位置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { getConfig } from 'utils';
+import { getConfig } from '@/utils';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
---
+++ src/utils/request.ts
@@ -0,0 +1,69 @@
+import axios, { Method } from 'axios';
+import { getConfig } from '@/utils';
+
+const {
+  FEISHU_CONFIG: { FEISHU_URL },
+} = getConfig();
+
+/**
+ * @description: 任意请求
+ */
+const request = async ({ url, option = {} }) => {
+  return axios.request({
+    url,
+    ...option,
+  });
+};
+
+interface IMethodV {
+  url: string;
+  method?: Method;
+  headers?: { [key: string]: string };
+  params?: Record<string, unknown>;
+  query?: Record<string, unknown>;
+}
+
+export interface IRequest {
+  data: any;
+  code: number;
+}
+
+/**
+ * @description: 带 version 的通用 api 请求
+ */
+const methodV = async ({
+  url,
+  method,
+  headers,
+  params = {},
+  query = {},
+}: IMethodV): Promise<IRequest> => {
+  let sendUrl = '';
+  if (/^(http:\/\/|https:\/\/)/.test(url)) {
+    sendUrl = url;
+  } else {
+    sendUrl = `${FEISHU_URL}${url}`;
+  }
+  return new Promise((resolve, reject) => {
+    axios({
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        ...headers,
+      },
+      url: sendUrl,
+      method,
+      params: query,
+      data: {
+        ...params,
+      },
+    })
+      .then(({ data, status }) => {
+        resolve({ data, code: status });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+export { request, methodV };
